a729892d564e3488fedd883e394383a89153ee0c
XaroRSA <Kennethdewittsi@gmail.com>
2016-10-11 19:46:28 +0200

    tewat

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 echo "stopping running application"
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
 
 echo "pulling latest version of the code"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull parabuzzle/devopsdayssv-webapp:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull xarorsa/grafana:latest'
 
 echo "starting the new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 80:4567 parabuzzle/devopsdayssv-webapp:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 80:3000 xarorsa/grafana:latest'
 
 echo "success!"
 
