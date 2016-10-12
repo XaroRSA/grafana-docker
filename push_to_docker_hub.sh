089398a76b8866949745b4b94ec99b7694a5f440
XaroRSA <Kennethdewittsi@gmail.com>
2016-10-12 12:29:27 +0200

    fix?

--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -8,15 +8,3 @@ if [ -z "${_grafana_tag}" ]; then
 else
 	docker push xarorsa/grafana:${_grafana_tag}
 fi
-
-echo "stopping running application"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
-
-echo "pulling latest version of the code"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull xarorsa/grafana:latest'
-
-echo "starting the new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 80:3000 xarorsa/grafana:latest'
-
-echo "success!"
